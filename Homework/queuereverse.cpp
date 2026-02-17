0ac4a62259aa5529b0ce417cd583980ed979170c
Anuj Ingole <anujingole01@gmail.com>
2026-02-17 23:55:25 +0530

    gfg queue

---
+++ Homework/queuereverse.cpp
@@ -0,0 +1,19 @@
+#include <queue>
+#include <stack>
+using namespace std;
+
+queue<int> rev(queue<int> q) {
+    stack<int> st;
+
+    while(!q.empty()) {
+        st.push(q.front());
+        q.pop();
+    }
+
+    while(!st.empty()) {
+        q.push(st.top());
+        st.pop();
+    }
+
+    return q;
+}
